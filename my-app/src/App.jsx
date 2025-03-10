36e41c86e4164a12315a01998f648b90e1d1260e
Bethel Zegeye <zegeyebethel@gmail.com>
2025-03-10 01:13:47 -0500

    Back_button update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Flashcard from './components/Flashcard'
+import data from './data/data.json'
 
 function App() {
 
@@ -8,10 +9,10 @@ function App() {
       <div className='header'>
         <h1>The Ultimate Plant Parent!</h1>
         <p>How good of a plant parent are you? Test all of your planty knowledge here!</p>
-        <h4>Number of cards: 10</h4>
+        <h4>Number of cards: {data.length - 1}</h4>
       </div>
       <div>
-        <Flashcard/>
+        <Flashcard data={data}/>
       </div>
 
 
--- my-app/src/components/Flashcard.jsx
+++ my-app/src/components/Flashcard.jsx
@@ -1,26 +1,42 @@
 import {useState} from 'react';
+import './flashcard.css'
 
-function Flashcard() {
-    const dict = [['ca','3'], ['c4','53'],['c','31']]
+function Flashcard({data}) {
     const [indx, setIndx] = useState(0)
     const [flip, setFlip] = useState(true)
+    const [stck, setStck] = useState([])
+    // const [start, setStart] = useState(true)
     function handleFlip() {
-        setFlip((prev) => (!prev))
+        setFlip(prev => (!prev))
+        setFlip(!flip)
     }
 
     function handleNext() {
-        setIndx((prev) => (prev + 1) % dict.length)
+        // setStart(false);
+        setIndx(Math.floor(Math.random() * (data.length - 1)) + 1)
         setFlip(true);
+        setStck(prev => [...prev, indx])
         
     }
+    function handleBack() {
+        if (stck.length > 0) {
+            const prevIndex = stck[stck.length - 1]; 
+            setStck(prev => prev.slice(0, -1));
+            setIndx(prevIndex); 
+            setFlip(true); 
+        }
+    }
+    
     return(
-
-        <div className="flashcard" onClick={handleFlip}>
-            <h1>{flip ? dict[indx][0] : dict[indx][1]}</h1>
-            <button onClick={(e) => { e.stopPropagation(); handleNext(); }}>{'->'}</button>        
-
-
+        <div>
+            <div className="flashcard" onClick={handleFlip}>
+                <h1>{flip ? data[indx][0] : data[indx][1]}</h1>
+            </div>
+            <button className='btn' onClick={(e) => { e.stopPropagation(); handleBack(); }}>{'<-'}</button> 
+            <button className='btn' onClick={(e) => { e.stopPropagation(); handleNext(); }}>{'->'}</button>
         </div>
+        
+        
     
     )
 
